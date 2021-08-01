06420ab02f9c5a929da470980d5b4a4797e076ca
Gilboot <ssenyonjogilbert@gmail.com>
2021-08-01 11:34:40 +0300

    reducer, todosSlice, filtersSlice

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,37 @@
+const initialState = [
+    { id: 0, text: 'Learn redux', completed: true },
+    { id: 1, text: 'Learn Redux', completed: false },
+    { id: 2, text: 'Building something fun', completed: false }
+]
+
+function nextTodoId(todos) {
+    const maxId = todos.reduce(function(maxSoFar, currTodo) {
+        return Math.max(maxSoFar, currTodo.id)
+    }, -1);
+    return maxId + 1;
+}
+
+export default function todosReducer(state = initialState, action) {
+    switch(action.type) {
+        case 'todos/todoAdded': {
+            return [
+                ...state,
+                {
+                    id: nextTodoId(state.todos),
+                    text: action.payload,
+                    completed: false
+                }
+            ]
+        }
+        case 'todos/todoToggled': {
+            return state.map(function(todo) {
+                if(todo.id === action.payload) {
+                    return { ...todo, completed: !todo.completed };
+                }
+                return todo;
+            });
+        }
+        default:
+            return state;
+    }
+}
